2a331bf92a56ed706329fc4e7589c6deebdf26a4
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-08-17 19:20:48 +0200

    [addon] Pick the cheap grey so its ready to throw

--- GreyHandling.lua
+++ GreyHandling.lua
@@ -70,6 +70,8 @@ local function GlowCheapestGrey()
 		SetBagItemGlow(bagNumNow, slotNumNow, "bags-glow-orange")
 		if bagNumNow==bagNumFuture and slotNumNow==slotNumFuture then
 			print("Cheapest:", GetContainerItemLink(bagNumNow, slotNumNow), GetCoinTextureString(minPriceNow))
+			PickupContainerItem(bagNumNow,slotNumNow);
+			print("Use left click to throw or exchange")
 		else
 			print("Cheapest now:", GetContainerItemLink(bagNumNow, slotNumNow), GetCoinTextureString(minPriceNow))
 			print("Cheapest later:", currentNumberFuture, "*", GetContainerItemLink(bagNumFuture, slotNumFuture), GetCoinTextureString(currentPriceFuture),
