824c3a25df35d8ad65d3f6cce6acba02b94fd1a9
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-08-21 20:41:10 +0200

    OneBag3 integration

--- functions/SetBagItemGlow.lua
+++ functions/SetBagItemGlow.lua
@@ -2,10 +2,15 @@ local A, GreyHandling = ...
 
 
 local function SetBagItemGlow(bagID, slotID, color)
-	for i = 1, NUM_CONTAINER_FRAMES, 1 do
-		local frame = _G["ContainerFrame"..i]
-		if frame:GetID() == bagID and frame:IsShown() then
-			item = _G["ContainerFrame"..i.."Item"..(GetContainerNumSlots(bagID) + 1 - slotID)]
+	local item = nil
+	if IsAddOnLoaded("OneBag3") then
+		item = _G["OneBagFrameBag"..bagID.."Item"..slotID]
+	else
+		for i = 1, NUM_CONTAINER_FRAMES, 1 do
+			local frame = _G["ContainerFrame"..i]
+			if frame:GetID() == bagID and frame:IsShown() then
+				item = _G["ContainerFrame"..i.."Item"..(GetContainerNumSlots(bagID) + 1 - slotID)]
+			end
 		end
 	end
 	if item then
