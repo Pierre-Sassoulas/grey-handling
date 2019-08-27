78bdef6dd081fb6a1b4b9dcf6c2aae4660e2fa35
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-08-27 07:34:12 +0200

    Create the option panel after the addon is loaded

--- GreyHandlingOptions.lua
+++ GreyHandlingOptions.lua
@@ -2,18 +2,8 @@ local A, GreyHandling = ...
 
 GreyHandling.options.panel = CreateFrame("Frame", nil, InterfaceOptionsFramePanelContainer);
 GreyHandling.options.panel.name = GreyHandling.DISPLAY_NAME
-local title = GreyHandling.options.panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
-title:SetPoint("TOPLEFT", 16, -16)
-title:SetText(GreyHandling.DISPLAY_NAME)
-local description = GreyHandling.options.panel:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
-description:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
-description:SetText(GreyHandling.DESCRIPTION)
-description:SetJustifyH("LEFT")
-description:SetJustifyV("TOP")
 
--- Variable for easy positioning
-lastcheckbox = description
-local function CreateCheckbox(text, tooltip, script)
+local function CreateCheckbox(text, tooltip, script, truth)
     -- Thanks to BattlePetBreedId author (Simca@Malfurion and Hugh@Burning Blade)
     local checkbox = CreateFrame("CheckButton", nil, GreyHandling.options.panel, "UICheckButtonTemplate")
     checkbox:SetPoint("TOPLEFT", lastcheckbox, "BOTTOMLEFT", 0, 0)
@@ -22,6 +12,7 @@ local function CreateCheckbox(text, tooltip, script)
     checkbox.tooltip = tooltip
     checkbox:SetScript("OnClick", script);
     checkbox.text:SetText(" " .. text)
+	checkbox:SetChecked(truth)
     lastcheckbox = checkbox
     return checkbox
 end
@@ -37,23 +28,6 @@ end
 local function change_value_show_price()
     GreyHandlingShowPrice = not GreyHandlingShowPrice
 end
-
--- definition order matter here lastcheckbox is global!
-local CheckboxTalkative = CreateCheckbox(
-    "Automatically offer to trade the grey item in chat (displayed to everyone)",
-    "",
-    change_value_talkative
-)
-local CheckboxVerbose = CreateCheckbox(
-    "Explain the logic behind the two chepeast items (displayed to you only)",
-    "",
-    change_value_verbose
-)
-local CheckboxShowPrice = CreateCheckbox(
-    "Display vendor sell prices in item tooltips (might be redondant with another addon)",
-    "",
-    change_value_show_price
-)
 
 function GreyHandling.options.display()
 	if GreyHandlingIsTalkative then
@@ -97,6 +71,29 @@ function GreyHandling.options.frame:OnEvent(event, key)
 		if GreyHandlingShowPrice == nil then
 			GreyHandlingShowPrice = GreyHandling.options.DEFAULT_SHOW_PRICE
 		end
+		local title = GreyHandling.options.panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		title:SetPoint("TOPLEFT", 16, -16)
+		title:SetText(GreyHandling.DISPLAY_NAME)
+		local description = GreyHandling.options.panel:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		description:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
+		description:SetText(GreyHandling.DESCRIPTION)
+		description:SetJustifyH("LEFT")
+		description:SetJustifyV("TOP")
+		-- Variable for easy positioning
+		lastcheckbox = description
+		-- definition order matter here lastcheckbox is global!
+		local CheckboxTalkative = CreateCheckbox(
+			"Automatically offer to trade the grey item in chat (displayed to everyone)", "", change_value_talkative,
+			GreyHandlingIsTalkative
+		)
+		local CheckboxVerbose = CreateCheckbox(
+			"Explain the logic behind the two chepeast items (displayed to you only)", "", change_value_verbose,
+			GreyHandlingIsVerbose
+		)
+		local CheckboxShowPrice = CreateCheckbox(
+			"Display vendor sell prices in item tooltips (might be redondant with another addon)", "",
+			change_value_show_price, GreyHandlingShowPrice
+		)
 	end
 	if event == "PLAYER_LOGOUT" then
 		GreyHandlingIsTalkative = GreyHandlingIsTalkative
