45867c8e5ac3f41cc00dd7cfff0f978ddf0889d0
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-08-31 14:17:35 +0200

    Handle nil player_id for chinese realm chat loot message
    Should permit that #9 does not crash and print a
    text in chat in a limited number of case.

--- functions/handleChatMessageLoot.lua
+++ functions/handleChatMessageLoot.lua
@@ -21,7 +21,6 @@ function GreyHandling.functions.handleChatMessageLoot(chat_message, player_name,
 	if GreyHandling.data.ourName == player_name then
 		return -- We can get the player items reliably with GetItemCount()
 	end
-	GreyHandling.functions.updatePlayersList(player_id, player_name)
 	-- chat_message contain an item link, and work like one as of patch 8.2.0
 	local _, itemLink, _, _, _, _, _, itemStackCount, _, _, vendorPrice, _, _, bindType = GetItemInfo(chat_message)
 	if itemStackCount == 1 or bindType == 1 or bindType == 4 then
@@ -30,6 +29,12 @@ function GreyHandling.functions.handleChatMessageLoot(chat_message, player_name,
 		-- No one want to trade quest item
 		return
 	end
+	if not player_id then
+		print(format("%s: We did not manage to understand '%s'", GreyHandling.NAME, chat_message))
+		print(format("%s: If this seem important to track loot properly, please report it on github.", GreyHandling.NAME))
+		return
+	end
+	GreyHandling.functions.updatePlayersList(player_id, player_name)
 	if GreyHandling.data.items[player_id][itemLink] == nil then
 		GreyHandling.functions.initPlayerItem(player_id, itemLink, itemStackCount, vendorPrice)
 	end
