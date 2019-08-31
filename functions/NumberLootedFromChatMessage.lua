0f5de5d521735fd30347f7bb211c9b66d9c0843c
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-08-31 18:30:35 +0200

    Fix parsing bug in chat loot message for chinese language

--- functions/NumberLootedFromChatMessage.lua
+++ functions/NumberLootedFromChatMessage.lua
@@ -1,11 +1,20 @@
 local A, GreyHandling = ...
 
 function GreyHandling.functions.numberLootedFromChatMessage(chat_message)
-	local position = string.find(chat_message, " x")
+	local position = string.find(chat_message, "]")
 	if position == nil then
 		return 1
 	end
-	local number = tonumber(string.sub(chat_message, position+2))
+	local numberStartIndex = position
+	for i=position, GreyHandling.MAX_CHAR_IN_CHAT_LOOT_MESSAGE, 1 do
+		if tonumber(string.sub(chat_message, i, i)) ~= nil then
+			-- TODO if the character is a number what is at his left is a number
+			-- This won't work for language that work from the right to the left
+			numberStartIndex = i
+			break
+		end
+	end
+	local number = tonumber(string.sub(chat_message, numberStartIndex))
 	if not number then
 		local error_msg = "Mutually beneficial trades will be less accurate. You can report this on Github."
 		print(format("%s: Failed to parser a number in '%s', assuming it's 1. %s", GreyHandling.NAME, chat_message, error_msg))
