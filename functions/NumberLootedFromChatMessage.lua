75150eed0f476e17b6a6e3b970cf28510de72db2
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-08-31 09:11:40 +0200

    Add a message for parsing fail in chat message
    Should help with #9 to make it unreliable but not outward
    crashing anymore.

--- functions/NumberLootedFromChatMessage.lua
+++ functions/NumberLootedFromChatMessage.lua
@@ -5,5 +5,11 @@ function GreyHandling.functions.numberLootedFromChatMessage(chat_message)
 	if position == nil then
 		return 1
 	end
-	return tonumber(string.sub(chat_message, position+2))
+	local number = tonumber(string.sub(chat_message, position+2))
+	if not number then
+		local error_msg = "Mutually beneficial trades will be less accurate. You can report this on Github."
+		print(format("%s: Failed to parser a number in '%s', assuming it's 1. %s", GreyHandling.NAME, chat_message, error_msg))
+		return 1
+	end
+	return number
 end
