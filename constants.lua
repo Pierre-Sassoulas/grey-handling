2719c7cf2bb8a6cfa707888a920d4e2649ac08d1
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-08-31 08:29:39 +0200

    Remove useless init in constants.lua

--- constants.lua
+++ constants.lua
@@ -21,7 +21,6 @@ local realmName=GetRealmName()
 GreyHandling.data.ourName = format("%s-%s", ourName, string.gsub(realmName, "%s+", ""))
 GreyHandling.data.names = {}
 GreyHandling.data.items = {}
-GreyHandling.announcedScrap = nil
 
 -- Test data remove in production
 --[[
