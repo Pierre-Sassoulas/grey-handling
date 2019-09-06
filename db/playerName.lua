1483730044b7f8bd2568f2e61a17cf4f9cec612d
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-09-06 13:43:21 +0200

    Possibility to have name with or without server on both version

--- db/playerName.lua
+++ db/playerName.lua
@@ -2,6 +2,8 @@ local A, GreyHandling = ...
 
 local playerName=UnitName("player")
 local realmName=GetRealmName()
+GreyHandling.data.playerNameWithoutServer = playerName
+GreyHandling.data.playerNameWithServer = format("%s-%s", playerName, string.gsub(realmName, "%s+", ""))
 if GreyHandling.IS_CLASSIC then
   GreyHandling.data.playerName = playerName
 else
