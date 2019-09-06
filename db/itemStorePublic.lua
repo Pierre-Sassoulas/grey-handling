2198f3eb412dff6fb094f58ff5cbd3171d2e4b03
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-09-06 13:43:55 +0200

    Create a db.setItemForPlayer function

--- db/itemStorePublic.lua
+++ db/itemStorePublic.lua
@@ -9,14 +9,18 @@ function GreyHandling.db.setItemInfoForPlayer(playerName, itemLink, infoName, va
     GreyHandling.data.items[playerName][itemLink][infoName] = value
 end
 
-function GreyHandling.db.addItemForPlayer(playerName, itemLink, vendorPrice, itemStackCount, number)
+function GreyHandling.db.setItemForPlayer(playerName, itemLink, vendorPrice, itemStackCount, number)
     -- print("In GreyHandling.db.addItemForPlayer :", playerName, itemLink, vendorPrice, itemStackCount, number)
     if not GreyHandling.db.playerItemInitialized(playerName, itemLink) then
         GreyHandling.db.initializeItem(playerName, itemLink, vendorPrice, itemStackCount)
     end
+    GreyHandling.db.setItemInfoForPlayer(playerName, itemLink, "number", number)
+end
+
+function GreyHandling.db.addItemForPlayer(playerName, itemLink, vendorPrice, itemStackCount, number)
+    if not GreyHandling.db.playerItemInitialized(playerName, itemLink) then
+        GreyHandling.db.initializeItem(playerName, itemLink, vendorPrice, itemStackCount)
+    end
     local old_number = GreyHandling.db.getItemInfoForPlayer(playerName, itemLink, "number")
-    GreyHandling.db.setItemInfoForPlayer(
-        playerName, itemLink, "number",
-        old_number + number
-    )
+    GreyHandling.db.setItemInfoForPlayer(playerName, itemLink, "number", old_number + number)
 end
