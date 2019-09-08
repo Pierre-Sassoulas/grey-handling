511b1119f284fe74fbf6f9b9466dd38066ca0264
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-09-08 14:06:59 +0200

    Uniformize the player name, sometime we have the server given on classic

--- db/itemStorePrivate.lua
+++ db/itemStorePrivate.lua
@@ -1,14 +1,26 @@
 local A, GreyHandling = ...
 
+function GreyHandling.db.uniformizePlayerName(playerName)
+	if GreyHandling.IS_CLASSIC and not string.find(playerName, "-") then
+		local realmName=GetRealmName()
+		playerName = format("%s-%s", playerName, string.gsub(realmName, "%s+", ""))
+	end
+	return playerName
+end
+
+
 function GreyHandling.db.playerItemInitialized(playerName, itemLink)
+	playerName = GreyHandling.db.uniformizePlayerName(playerName)
     return GreyHandling.data.items[playerName] and GreyHandling.data.items[playerName][itemLink]
 end
 
 function GreyHandling.functions.playerHasItem(playerName, itemLink)
+	playerName = GreyHandling.db.uniformizePlayerName(playerName)
     return GreyHandling.db.playerItemInitialized(playerName, itemLink) and GreyHandling.db.getItemInfoForPlayer(playerName, itemLink, "number") > 0
 end
 
 function GreyHandling.db.initializePlayer(playerName, bagSpace)
+	playerName = GreyHandling.db.uniformizePlayerName(playerName)
 	if not GreyHandling.data.items[playerName] then
 		bagSpace = bagSpace or 181 -- 36*5 + 1
 		GreyHandling.data.items[playerName] = {bagSpace=bagSpace}
@@ -17,6 +29,7 @@ end
 
 function GreyHandling.db.initializeItem(playerName, itemLink, vendorPrice, itemStackCount)
 	-- print(playerName, itemLink, vendorPrice, itemStackCount)
+	playerName = GreyHandling.db.uniformizePlayerName(playerName)
 	GreyHandling.db.initializePlayer(playerName)
 	if not GreyHandling.data.items[playerName][itemLink] then
 		GreyHandling.data.items[playerName][itemLink] = {
