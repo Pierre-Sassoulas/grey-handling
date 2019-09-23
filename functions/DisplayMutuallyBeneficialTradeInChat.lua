44a66b3313da42f877083ea9b89f4769069aebe3
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-09-23 13:21:07 +0200

    Better message for exchange

--- functions/DisplayMutuallyBeneficialTradeInChat.lua
+++ functions/DisplayMutuallyBeneficialTradeInChat.lua
@@ -2,11 +2,11 @@ local A, GreyHandling = ...
 
 function GreyHandling.functions.DisplayMutuallyBeneficialTradeInChat(exchange)
 	local multiplicator = "*"
-	local msg = "|cFF6699ff"..exchange.playerId.."|r - "..exchange.itemGiven..multiplicator..exchange.ourCount
+	local msg = ""..exchange.itemGiven..multiplicator..exchange.ourCount
 	if exchange.ourGain > 0 then
 		msg = format("%s and %s", msg, GetCoinTextureString(exchange.ourGain))
 	end
-	msg = msg.." for their "..exchange.itemTaken..multiplicator..exchange.theirCount
+	msg = msg.." for |cFF6699ff"..exchange.playerId.."|r's "..exchange.itemTaken..multiplicator..exchange.theirCount
 	if exchange.ourGain < 0 then
 		msg = format("%s and %s", msg, GetCoinTextureString(-exchange.ourGain))
 	end
