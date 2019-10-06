b611ba386f0e1963e1ecd30805747c69884d23bc
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-10-06 17:16:54 +0200

    Separate text generation and display

--- functions/DisplayCheapestInChat.lua
+++ functions/DisplayCheapestInChat.lua
@@ -1,19 +1,23 @@
 local A, GreyHandling = ...
 
+function GreyHandling.functions.GetCheapestMessage(text, item)
+	local details = ""
+	if item.itemCount == 1 then
+		details = GetContainerItemLink(item.bag, item.slot)
+	elseif item.potentialPrice == item.currentPrice then
+		details = format("A full stack of %s", GetContainerItemLink(item.bag, item.slot))
+	else
+		details = format("%s %s/%s",
+			GetContainerItemLink(item.bag, item.slot),
+			item.itemCount,
+			item.itemStackCount
+		)
+	end
+	return format("%s %s (%s)", text, details, GetCoinTextureString(item.currentPrice))
+end
+
 function GreyHandling.functions.DisplayCheapestInChat(text, item)
 	if GreyHandlingIsVerbose then
-		local details = ""
-		if item.itemCount == 1 then
-			details = GetContainerItemLink(item.bag, item.slot)
-		elseif item.potentialPrice == item.currentPrice then
-			details = format("A full stack of %s", GetContainerItemLink(item.bag, item.slot))
-		else
-			details = format("%s %s/%s",
-				GetContainerItemLink(item.bag, item.slot),
-				item.itemCount,
-				item.itemStackCount
-			)
-		end
-		GreyHandling.print(format("%s %s (%s)", text, details, GetCoinTextureString(item.currentPrice)))
+		GreyHandling.print(GreyHandling.functions.GetCheapestMessage(text, item))
 	end
 end
