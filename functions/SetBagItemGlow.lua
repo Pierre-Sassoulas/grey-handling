a23c49dd4007d2d891b530136daf3694f23cec8f
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-10-07 22:27:38 +0200

    Rename item to itemFrame

--- functions/SetBagItemGlow.lua
+++ functions/SetBagItemGlow.lua
@@ -2,26 +2,27 @@ local A, GreyHandling = ...
 
 
 function GreyHandling.functions.SetBagItemGlow(bagID, slotID, color)
+	local itemFrame
 	if IsAddOnLoaded("OneBag3") then
-		item = _G["OneBagFrameBag"..bagID.."Item"..slotID]
+		itemFrame = _G["OneBagFrameBag"..bagID.."Item"..slotID]
 	elseif IsAddOnLoaded("Inventorian") then
 		return
     elseif IsAddOnLoaded("ArkInventory") or IsAddOnLoaded("ArkInventoryClassic") then
         local loc_id, bag_id = ArkInventory.BlizzardBagIdToInternalId(bagID)
-        local framename, item = ArkInventory.API.ItemFrameGet(loc_id, bag_id, slotID)
-        ActionButton_ShowOverlayGlow(item)
+        local framename, itemFrame = ArkInventory.API.ItemFrameGet(loc_id, bag_id, slotID)
+        ActionButton_ShowOverlayGlow(itemFrame)
 	else
 		for i = 1, NUM_CONTAINER_FRAMES, 1 do
 			local frame = _G["ContainerFrame"..i]
 			if frame:GetID() == bagID and frame:IsShown() then
-				item = _G["ContainerFrame"..i.."Item"..(GetContainerNumSlots(bagID) + 1 - slotID)]
+				itemFrame = _G["ContainerFrame"..i.."Item"..(GetContainerNumSlots(bagID) + 1 - slotID)]
 			end
 		end
 	end
-	if item then
-		item.NewItemTexture:SetAtlas(color)
-		item.NewItemTexture:Show()
-		item.flashAnim:Play()
-		item.newitemglowAnim:Play()
+	if itemFrame then
+		itemFrame.NewItemTexture:SetAtlas(color)
+		itemFrame.NewItemTexture:Show()
+		itemFrame.flashAnim:Play()
+		itemFrame.newitemglowAnim:Play()
 	end
 end
