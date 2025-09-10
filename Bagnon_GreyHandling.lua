afaa2278bef4a1fc84f4ee0dfc6874307e2a0669
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-09-10 15:13:24 +0200

    Calculate the best exchange less often by caching the value

--- Bagnon_GreyHandling.lua
+++ Bagnon_GreyHandling.lua
@@ -23,6 +23,47 @@ if IsAddOnLoaded("Bagnon") or IsAddOnLoaded("Combuctor") then
 	local UpdateBorder = ItemSlot.UpdateBorder
 	local r, g, b = GetItemQualityColor(0)
 
+	-- OPTIMIZATION: Simple caching - only recalculate when data actually changes
+	local cachedBestExchanges = nil
+
+	-- Function to get cached best exchanges
+	local function GetCachedBestExchanges()
+		if cachedBestExchanges == nil then
+			cachedBestExchanges = GreyHandling.functions.GetBestExchanges()
+		end
+		return cachedBestExchanges
+	end
+
+	-- Function to invalidate cache when data changes
+	local function InvalidateExchangeCache()
+		cachedBestExchanges = nil
+	end
+
+	-- Hook into functions that change the data to invalidate cache
+	local originalAddItemForPlayer = GreyHandling.db.addItemForPlayer
+	GreyHandling.db.addItemForPlayer = function(...)
+		InvalidateExchangeCache()
+		return originalAddItemForPlayer(...)
+	end
+
+	local originalSetItemForPlayer = GreyHandling.db.setItemForPlayer
+	GreyHandling.db.setItemForPlayer = function(...)
+		InvalidateExchangeCache()
+		return originalSetItemForPlayer(...)
+	end
+
+	local originalRemovePlayerThatLeft = GreyHandling.db.removePlayerThatLeft
+	GreyHandling.db.removePlayerThatLeft = function(...)
+		InvalidateExchangeCache()
+		return originalRemovePlayerThatLeft(...)
+	end
+
+	local originalRemovePlayer = GreyHandling.db.removePlayer
+	GreyHandling.db.removePlayer = function(...)
+		InvalidateExchangeCache()
+		return originalRemovePlayer(...)
+	end
+
 	Addon.Rules:New('greyhandling', 'Grey Handling', 'Interface\\Addons\\GreyHandling\\Art\\GreyHandlingIcon', function(player, bag, slot, bagInfo, itemInfo)
 		if itemInfo.id and bag and slot then
 			if GreyHandling.isCheapest(bag, slot) then
@@ -49,22 +90,46 @@ if IsAddOnLoaded("Bagnon") or IsAddOnLoaded("Combuctor") then
 			self.IconGlow:SetVertexColor(r, g, b, Addon.sets.glowAlpha)
 			self.IconGlow:Show()
 		end
+
+		-- Check if this is the cheapest item
 		local cheapest = GreyHandling.isCheapest(bag, slot)
 		if cheapest then
 			self.IconBorder:SetVertexColor(1, 0.1, 0.1)
 			self.IconBorder:SetTexture([[Interface\Artifacts\RelicIconFrame]])
 			self.IconBorder:Show()
-			--self.IconGlow:SetTexture('Interface\\Addons\\GreyHandling\\Art\\GreyHandlingIcon')
-			--self.IconGlow:Show()
 		end
-		local trade = GreyHandling.isMutuallyBeneficialTrade(bag, slot)
-		if trade then
-			self.IconBorder:SetVertexColor(0.1, 1, 0.1)
-			self.IconBorder:SetTexture([[Interface\Artifacts\RelicIconFrame]])
-			self.IconBorder:Show()
+
+		-- OPTIMIZATION: Use cached best exchanges instead of calling GetBestExchanges() every time
+		if bag and slot then
+			local bestExchanges = GetCachedBestExchanges()
+			local trade = false
+
+			-- Check if this item is part of a mutually beneficial trade
+			for i, exchange in pairs(bestExchanges) do
+				if exchange.itemGiven and exchange.itemTaken then
+					local exchangeBag, exchangeSlot = GreyHandling.functions.GetBagAndSlot(exchange.itemGiven, exchange.ourCount)
+					if exchangeBag == bag and exchangeSlot == slot then
+						trade = true
+						break
+					end
+				end
+			end
+
+			if trade then
+				self.IconBorder:SetVertexColor(0.1, 1, 0.1)
+				self.IconBorder:SetTexture([[Interface\Artifacts\RelicIconFrame]])
+				self.IconBorder:Show()
+			end
 		end
 	end
 
+	-- Hook into events that change player inventory to invalidate cache
+	local cacheInvalidationFrame = CreateFrame("Frame")
+	cacheInvalidationFrame:RegisterEvent("GROUP_LEFT")
+	cacheInvalidationFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	cacheInvalidationFrame:SetScript("OnEvent", function(self, event, ...)
+		InvalidateExchangeCache()
+	end)
 
 	--[[ Update Events ]]--
 
